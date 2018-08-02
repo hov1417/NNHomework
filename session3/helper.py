52c2bab23aa30d91ed9211a312765a5f14982fa7
Hovhannes.Sahakyan <hovhannes.sahakyan@synisys.com>
2018-08-02 14:29:36 +0400

    GAN completed

--- session3/helper.py
+++ session3/helper.py
@@ -1,5 +1,4 @@
 from IPython.display import Audio, display
-
 def allDone():
     display(Audio(url='https://sound.peal.io/ps/audios/000/000/537/original/woo_vu_luvub_dub_dub.wav', 
                   autoplay=True))
