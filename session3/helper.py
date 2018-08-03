ac8db11725709357c03ceb7f5fc968ce9a077800
Hovhannes.Sahakyan <hovhannes.sahakyan@synisys.com>
2018-08-03 21:02:48 +0400

    GAN on MNIST data, some template

--- session3/helper.py
+++ session3/helper.py
@@ -1,4 +1,10 @@
 from IPython.display import Audio, display
+import numpy as np
+
 def allDone():
     display(Audio(url='https://sound.peal.io/ps/audios/000/000/537/original/woo_vu_luvub_dub_dub.wav', 
                   autoplay=True))
+
+def describe(x):
+	x = np.asarray(x)
+	print(x.shape, x.min(), x.mean(), x.max())
